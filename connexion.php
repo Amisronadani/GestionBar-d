e761ad0c643af7de491af9d20015e81ce0e680a5
Kevin-Blanco <khakizimana73@gmail.com>
2026-02-13 10:08:13 +0200

    Ajout du chaine de connexion

--- connexion.php
+++ connexion.php
@@ -1,6 +1,15 @@
 <?PHP 
-
-
-
-
+$hostname="localhost";
+username="root";
+$password="";  
+try{
+    $conn = new PDO("mysql:host=$hostname;dbname=gestion", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully"; 
+    }
+catch(PDOException $e)
+    {
+    echo "Connection failed: " . $e->getMessage();
+}
 ?>
